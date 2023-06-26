3d3014e6bd5f1f2aa13c73b8885ddaa72c692bd1
ErickUrbizagastegui <89022087+ErickUrbizagastegui@users.noreply.github.com>
2023-06-26 11:41:52 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) { //DEBUGGEAR
+        
         PlanDePagos plan = new PlanDePagos();
         Ejecutar ejecutar = new Ejecutar(plan);
         ejecutar.correr();
         ejecutar.imprimirInformacion();
+        
     }
 }
